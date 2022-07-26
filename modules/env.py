0572721f15be739bf9db444560cbe9fcf4eb51d0
NoobMaster669 <NoobMaster669@protonmail.com>
2022-07-26 20:47:17 +0530

    fixing tests

--- modules/env.py
+++ modules/env.py
@@ -8,25 +8,25 @@ from distutils import util
 # any method starting with is will be boolean and returns False by default
 # TODO: should all these method be replaced with a single `is_enabled` method ??
 def is_auto_mode():
-    return bool(util.strtobool(os.getenv('auto_mode', "False")))
+    return bool(util.strtobool(str(os.getenv('auto_mode', False))))
 
 def is_force_auto_upload():
-    return bool(util.strtobool(os.getenv('force_auto_upload', "False")))
+    return bool(util.strtobool(str(os.getenv('force_auto_upload', False))))
 
 def is_check_dupes():
-    return bool(util.strtobool(os.getenv('check_dupes', "False")))
+    return bool(util.strtobool(str(os.getenv('check_dupes', False))))
 
 def is_containerized():
-    return bool(util.strtobool(os.getenv("IS_CONTAINERIZED", "False")))
+    return bool(util.strtobool(str(os.getenv("IS_CONTAINERIZED", False))))
 
 def is_full_disk_supported():
-    return bool(util.strtobool(os.getenv("IS_FULL_DISK_SUPPORTED", "False")))
+    return bool(util.strtobool(str(os.getenv("IS_FULL_DISK_SUPPORTED", False))))
 
 def is_live():
-    return bool(util.strtobool(os.getenv('live', "False")))
+    return bool(util.strtobool(str(os.getenv('live', False))))
 
 def is_readble_temp_data_needed():
-    return bool(util.strtobool(os.getenv("readable_temp_data", "False")))
+    return bool(util.strtobool(str(os.getenv("readable_temp_data", False))))
 
 def get_image_host_by_priority(priority, default=None):
     return os.getenv(f'img_host_{priority}', default)
@@ -60,7 +60,7 @@ def get_property_or_default(env_key, default=None):
 
 # Translation properties
 def is_translation_needed():
-    return bool(util.strtobool(os.getenv('translation_needed', "False")))
+    return bool(util.strtobool(str(os.getenv('translation_needed', False))))
 
 def get_uploader_accessible_path(default=''):
     return os.getenv('uploader_accessible_path', default)
@@ -72,7 +72,7 @@ def get_client_accessible_path(default=''):
 
 # Screenshots properties
 def is_no_spoiler_screenshot():
-    return bool(util.strtobool(os.getenv("no_spoilers", "False")))
+    return bool(util.strtobool(str(os.getenv("no_spoilers", False))))
 
 def get_imgur_client_id(default=None):
     return os.getenv('imgur_client_id', default)
@@ -93,10 +93,10 @@ def get_thumb_size():
 
 # Post Processing properties
 def is_post_processing_needed():
-    return bool(util.strtobool(os.getenv('enable_post_processing', "False")))
+    return bool(util.strtobool(str(os.getenv('enable_post_processing', False))))
 
 def is_type_based_move_enabled():
-    return bool(util.strtobool(os.getenv('enable_type_base_move', "False")))
+    return bool(util.strtobool(str(os.getenv('enable_type_base_move', False))))
 
 def get_post_processing_mode(default=''):
     return os.getenv('post_processing_mode', default)
@@ -129,7 +129,7 @@ def get_client_path():
     return os.getenv("client_path", "/")
 
 def is_dynamic_tracker_selection_needed():
-    return bool(util.strtobool(os.getenv("dynamic_tracker_selection", "False")))
+    return bool(util.strtobool(str(os.getenv("dynamic_tracker_selection", False))))
 
 def get_reupload_label():
     return os.getenv('reupload_label', '')
