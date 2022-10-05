69f45bbe02ce6876f0d018814f83ac4ea35bfc8e
NoobMaster669 <NoobMaster669@protonmail.com>
2022-10-05 21:08:05 +0530

    comments

--- dev_scripts/validate_templates.py
+++ dev_scripts/validate_templates.py
@@ -1,3 +1,19 @@
+# GG Bot Upload Assistant
+# Copyright (C) 2022  Noob Master669
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 import json
 import fastjsonschema
@@ -5,6 +21,9 @@ import fastjsonschema
 from pathlib import Path
 from fastjsonschema import JsonSchemaException
 
+
+# the schemas can also be validated at https://www.jsonschemavalidator.net/
+# Note: ignore the uri validation error for `upload_form` and `torrents_search` key injsonschemavalidator
 working_folder = Path(__file__).resolve().parent.parent
 validate = fastjsonschema.compile(json.load(open(f"{working_folder}/schema/site_template_schema.json", "r")))
 
