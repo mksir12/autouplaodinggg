34c0370da1baf755709f0f941104485e9d76f2b0
NoobMaster669 <NoobMaster669@protonmail.com>
2023-04-18 22:13:51 +0530

    issue#110 GGBotException when connection to mongo fails bix

--- modules/exceptions/exception.py
+++ modules/exceptions/exception.py
@@ -1,6 +1,6 @@
 class GGBotException(Exception):
     def __init__(self, message):
-        super.__init__(message)
+        super().__init__(message)
 
 
 class GGBotUploaderException(GGBotException):
