9a07a547c433229defb2376a9ee92f3be48a00bf
NoobMaster669 <NoobMaster669@protonmail.com>
2023-04-21 19:08:24 +0530

    nbl custom action test

--- tests/modules/custom_actions/nbl_actions_test.py
+++ tests/modules/custom_actions/nbl_actions_test.py
@@ -16,7 +16,10 @@ class TestNBLCustomActions:
             pytest.param(
                 {"episode_number": "0"}, {"ignoredupes": "1"}, id="season_pack"
             ),
-            pytest.param({}, {"ignoredupes": "1"}, id="season_pack_2"),
+            pytest.param(
+                {"episode_number": 0}, {"ignoredupes": "1"}, id="season_pack_2"
+            ),
+            pytest.param({}, {"ignoredupes": "1"}, id="season_pack_3"),
         ],
     )
     def test_season_pack_dupe(self, torrent_info, expected):
