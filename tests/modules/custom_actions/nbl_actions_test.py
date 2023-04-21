402c52e43e2e6ce7b1d3c020ab90dec746c811e1
NoobMaster669 <NoobMaster669@protonmail.com>
2023-04-21 19:07:58 +0530

    nbl custom action test

---
+++ tests/modules/custom_actions/nbl_actions_test.py
@@ -0,0 +1,25 @@
+import pytest
+
+from modules.custom_actions.nbl_actions import season_pack_dupe
+
+
+class TestNBLCustomActions:
+    @pytest.mark.parametrize(
+        ("torrent_info", "expected"),
+        [
+            pytest.param(
+                {"episode_number": "1"}, {}, id="single_episode_release"
+            ),
+            pytest.param(
+                {"episode_number": 1}, {}, id="single_episode_release_2"
+            ),
+            pytest.param(
+                {"episode_number": "0"}, {"ignoredupes": "1"}, id="season_pack"
+            ),
+            pytest.param({}, {"ignoredupes": "1"}, id="season_pack_2"),
+        ],
+    )
+    def test_season_pack_dupe(self, torrent_info, expected):
+        tracker_settings = {}
+        season_pack_dupe(torrent_info, tracker_settings, None)
+        assert tracker_settings == expected
